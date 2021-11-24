9a617ed2ec47e0ed3ed23c1cb46ac11f2345d834
shamrylo <vitaliy.shamrylo@grandiz.com>
2021-11-24 14:52:58 +0200

    Version Change. Minimized File.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     mode: 'production',
     output: {
-        filename: 'wallkit-integration-library.js',
+        filename: 'wallkit-integration-library.min.js',
         path: path.resolve('dist'),
     },
     module: {
