8976d6eb4c1abcaa134b751a4fadd883422fb4e8
Vitalii Shamrylo <114018221+VitOrderCast@users.noreply.github.com>
2023-02-14 15:34:49 +0100

    Update JS SDK Version.

--- dist/wallkit-integration-library.js
+++ dist/wallkit-integration-library.js
@@ -5776,7 +5776,7 @@ var SDK = /*#__PURE__*/function () {
   }, {
     key: "load",
     value: function load() {
-      (0, _DOM.insertScript)("".concat(_constants.WALLKIT_CDN_URL, "/js/sdk/0.0.47/wallkit.umd.min.js"), 'wallkit-js-sdk', this.onLoad.bind(this));
+      (0, _DOM.insertScript)("".concat(_constants.WALLKIT_CDN_URL, "/js/sdk/0.0.48/wallkit.umd.min.js"), 'wallkit-js-sdk', this.onLoad.bind(this));
     }
   }]);
   return SDK;
