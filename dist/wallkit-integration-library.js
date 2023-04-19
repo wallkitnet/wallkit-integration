ebe93b5352a4481aa5f96b313669b8e1b063f45e
illyawebdev <72105915+illyawebdev@users.noreply.github.com>
2023-04-19 16:20:57 +0300

    Merge pull request #7
    * Password reset popup: changed texts.
    
    * Merge branch 'master' into feature/reset-password-text
    
    * build

--- dist/wallkit-integration-library.js
+++ dist/wallkit-integration-library.js
@@ -3995,12 +3995,12 @@ var AuthForm = /*#__PURE__*/function () {
     key: "showSuccessPasswordForgot",
     value: function showSuccessPasswordForgot() {
       var email = this.forgotPasswordForm.emailField.getValue();
-      this.forgotPasswordForm.showFormResult("\n            <div class=\"wk-success-message wk-password-reset-success wk-password-forgot-message\">\n                <p class=\"wk-success-message__description\">Please follow the instructions sent to <b>".concat(email, "</b> to recover your password</p>\n                <button id=\"back-to-login\" class=\"wk-form-button wk-form-button--cancel\">Back to login</button>\n            </div>\n        "));
+      this.forgotPasswordForm.showFormResult("\n            <div class=\"wk-success-message wk-password-reset-success wk-password-forgot-message\">\n                <p class=\"wk-success-message__description\">Please follow the instructions sent to <b>".concat(email, "</b> to set a new password.</p>\n                <button id=\"back-to-login\" class=\"wk-form-button wk-form-button--cancel\">Back to login</button>\n            </div>\n        "));
     }
   }, {
     key: "showSuccessPasswordReset",
     value: function showSuccessPasswordReset() {
-      this.resetPasswordForm.showFormResult("\n            <div class=\"wk-success-message wk-password-reset-success wk-password-reset-message\">\n                <h2 class=\"wk-success-message__title\">Your password has been successfully changed!</h2>\n                <button id=\"back-to-login\" class=\"wk-form-button wk-form-button--cancel\">Back to login</button>\n            </div>\n        ");
+      this.resetPasswordForm.showFormResult("\n            <div class=\"wk-success-message wk-password-reset-success wk-password-reset-message\">\n                <h2 class=\"wk-success-message__title\">Your password had been saved successfully!</h2>\n                <button id=\"back-to-login\" class=\"wk-form-button wk-form-button--cancel\">Back to login</button>\n            </div>\n        ");
     }
   }, {
     key: "render",
