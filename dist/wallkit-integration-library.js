c7a67d6b33adbf14942649cdedcc0b69eca7fc7b
Illya Baskov <illya.baskov@grandiz.com>
2023-06-20 15:47:46 +0300

    new version v3.0.16

--- dist/wallkit-integration-library.js
+++ dist/wallkit-integration-library.js
@@ -1,7 +1,7 @@
 /*!
  * Package name: wallkit-integration-lib.
  * Package description: Wallkit Integration Library. Library to manipulate with Wallkit System: Paywall, Modals, Authentication, SDK..
- * Package version: 3.0.15.
+ * Package version: 3.0.16.
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
