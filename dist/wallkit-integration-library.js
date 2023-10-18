cdeb0e1987e2b479a73048968b5a89a82b7f3855
illyawebdev <72105915+illyawebdev@users.noreply.github.com>
2023-10-18 16:48:45 +0300

    signUp form. checkbox id fix. (#29)

--- dist/wallkit-integration-library.js
+++ dist/wallkit-integration-library.js
@@ -5798,6 +5798,7 @@ var SignupForm = /*#__PURE__*/function (_Form) {
       _this.tosField = new _CheckBoxField.CheckBoxField({
         dataSlug: 'agreement',
         name: 'wk-fb-agreement',
+        id: 'wk-fb-agreement',
         required: true,
         label: _this.getTosAcceptLabel(_options.termsOfService),
         type: 'checkbox',
